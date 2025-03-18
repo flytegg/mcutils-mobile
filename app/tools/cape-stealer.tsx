41cb4b0bd94bc149cf00ffa5545863de190f9b2d
SamuSonoIo <samusonoio24@gmail.com>
2025-03-18 19:01:06 +0100

    (feat) Basic Cape-Stealer page structure

--- app/tools/cape-stealer.tsx
+++ app/tools/cape-stealer.tsx
@@ -1,9 +1,15 @@
+import { Input } from '@/components/Input'
 import React from 'react'
 
 export default function capestealer() {
   return (
     <>
+      <h1 className='text-center font-semibold text-3xl uppercase flex justify-center align-center flex-col p-14'>Cape Stealer</h1>
+      <div className='flex justify-center'>
+        <div className='w-96'>
+          <Input className='w-1/3' placeholder='Enter a username' />
+        </div>
+      </div>
     </>
   )
 }
-
